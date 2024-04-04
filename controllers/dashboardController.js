33976df11e095e04b5dcde43fe48356dbe763047
GokulGB77 <148517548+GokulGB77@users.noreply.github.com>
2024-04-04 15:56:05 +0530

    dashboard started

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,7 +1,29 @@
+const Userdb = require("../models/userModel")
+const Cartdb = require("../models/cartModel")
+const Addressdb = require("../models/addressModel")
+const Ordersdb = require("../models/ordersModel")
+const Productsdb = require("../models/productsModel")
+const Categoriesdb = require("../models/categoriesModel")
+const Walletdb = require("../models/walletModel")
+const Couponsdb = require("../models/couponsModel")
+
+
 const loadAdminDashoard = async (req, res) => {
   try {
-  
-    res.render("dashboard", {title: 'Admin Dashboard'});
+    const allProducts  =await Productsdb.find()
+    const allCategories  =await Categoriesdb.find()
+    const allOrders = await Ordersdb.find({orderStatus:"delivered"});
+
+    const totalOrderPriceSum = allOrders.reduce((accumulator, currentOrder) => {
+      const orderTotalPriceSum = currentOrder.orderProducts.reduce((productAccumulator, currentProduct) => {
+        return productAccumulator + currentProduct.totalPrice;
+      }, 0);
+      return accumulator + orderTotalPriceSum;
+    }, 0);
+
+    console.log("Total sum of all order products' totalPrice:", totalOrderPriceSum);
+
+    res.render("dashboard", { title: 'Admin Dashboard',allOrders, revenue:totalOrderPriceSum ,allProducts,allCategories});
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Internal Server Error");
